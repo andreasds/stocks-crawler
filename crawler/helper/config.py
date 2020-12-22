86c705de24eda7786cc7e45a4b198e321b61635c
Andreas Dharmawan <andreas.ds90@gmail.com>
2020-12-22 14:10:53 +0700

    add db config

--- crawler/helper/config.py
+++ crawler/helper/config.py
@@ -1,17 +1,40 @@
-def readUserConfig(configPath):
-  configKey = [ 'poemsUser', 'poemsPassword', 'poemsPin' ]
-  userConfig = {}
+def __readConfig(configPath, configName, configKey):
+  filterConfig = {}
 
   with open(configPath) as f:
     allConfig = f.readlines()
     for config in allConfig:
+      if config.strip().startswith('#') or \
+          config.strip() == '':
+        continue
+
       dictConfig = config.strip().split('=')
       if len(dictConfig) != 2:
-        raise ValueError('Invalid user config file')
+        raise ValueError('Invalid ' + configName + ' config file')
 
       if dictConfig[0].strip() not in configKey:
         continue
 
-      userConfig[dictConfig[0].strip()] = dictConfig[1].strip()
+      filterConfig[dictConfig[0].strip()] = dictConfig[1].strip()
+
+  return filterConfig
+
+def readUserConfig(configPath):
+  userKey = [ 'poemsUser', 'poemsPassword', 'poemsPin' ]
+  userConfig = __readConfig(configPath, 'user', userKey)
+
+  for key in userKey:
+    if key not in userConfig:
+      raise RuntimeError('Invalid user config file')
 
   return userConfig
+
+def readDbConfig(configPath):
+  dbKey = [ 'dbHost', 'dbPort', 'dbUser', 'dbPassword' ]
+  dbConfig = __readConfig(configPath, 'db', dbKey)
+
+  for key in dbKey:
+    if key not in dbConfig:
+      raise RuntimeError('Invalid db config file')
+
+  return dbConfig
